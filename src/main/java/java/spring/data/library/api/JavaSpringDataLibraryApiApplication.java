78b597dc4198297bb2ea6d1ee3cfb6f6778e799f
SergeiGorvat <gorvat2012@yandex.ru>
2026-03-02 16:16:50 +0300

    Initial commit

---
+++ src/main/java/java/spring/data/library/api/JavaSpringDataLibraryApiApplication.java
@@ -0,0 +1,13 @@
+package java.spring.data.library.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringDataLibraryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringDataLibraryApiApplication.class, args);
+    }
+
+}
